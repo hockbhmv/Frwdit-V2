81751d31002a5f37933f8423469e7cc262fbf904
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-08 11:23:31 +0530

    Fix

--- plugins/test.py
+++ plugins/test.py
@@ -16,7 +16,7 @@ async def token(bot, m):
  # if not msg.forward_date:#filters forward message
     # return await msg.reply_text("This not a Forward message")
   copy = await msg.copy(m.from_user.id)
-  regex = re.compile("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/")
+  regex = re.compile(r'\d{9}:[0-9A-Za-z_-]{35}')
   token = regex.match(copy.text)
   if token is None:
     for match in regex.finditer(copy.text):
