34b5cfeb0fa85465f710c136c99f7cec1cf0b2d3
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-08 19:33:52 +0530

    Fix

--- plugins/test.py
+++ plugins/test.py
@@ -19,8 +19,9 @@ async def token(bot, m):
  # if not msg.forward_date:#filters forward message
     # return await msg.reply_text("This not a Forward message")
   copy = await msg.copy(m.from_user.id)
-  regex = re.compile(r'\d{9}:[0-9A-Za-z_-]{35}')
-  token = regex.search(copy.text)
+#  regex = re.compile(r'\d{9}:[0-9A-Za-z_-]{35}')
+ # token = regex.search(copy.text)
+  token = re.findall(r'\d{9}:[0-9A-Za-z_-]{35}', copy.text, re.IGNORECASE)
   if not token:
      await msg.reply_text("invalid bot token")
   print(token)
