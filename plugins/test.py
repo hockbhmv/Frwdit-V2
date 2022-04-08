41c96c865930f4e63b2582a879bb504cfd1eaf6c
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-08 10:53:08 +0530

    Fix

--- plugins/test.py
+++ plugins/test.py
@@ -13,11 +13,16 @@ from translation import Translation
 @Client.on_message(filters.private & filters.command('add'))
 async def token(bot, m):
   msg = await bot.ask(chat_id=m.from_user.id, text="Forward the message from bot father")
-  if not msg.forward_date:#filters forward message
-     return await msg.reply_text("This not a Forward message")
+ # if not msg.forward_date:#filters forward message
+    # return await msg.reply_text("This not a Forward message")
   copy = await msg.copy(m.from_user.id)
-  #regex = re.compile(")
-  token = copy.text.match("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/")
+  regex = re.compile("/[0-9]{1,}:\w*/")
+  token = regex.match(copy.text)#("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/")
+  if token is None:
+    for match in regex.finditer(copy.text)
+        token = match 
+    else: 
+        await msg.reply_text(f"error on find bottoken")
   await msg.reply_text(f"your token :- \n{token}")
   return
     
