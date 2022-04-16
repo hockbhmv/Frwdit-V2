2b48e529e8b87d7e88d86837c53b6bbaab5ceda7
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-16 11:53:51 +0530

    Fix

--- plugins/test.py
+++ plugins/test.py
@@ -15,22 +15,13 @@ BOT_TOKEN = {}
 
 @Client.on_message(filters.private & filters.command('add'))
 async def token(bot, m):
-  msg = await bot.ask(chat_id=m.from_user.id, text="Forward the message from bot father")
- # if not msg.forward_date:#filters forward message
-    # return await msg.reply_text("This not a Forward message")
-  copy = await msg.copy(m.from_user.id)
-  regex = re.compile(r'\d{9}:[0-9A-Za-z_-]{35}')
- # token = regex.search(copy.text)
+  msg = await bot.ask(chat_id=m.from_user.id, text="1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me")
+  if not msg.forward_date and msg.forward_from.id=="93372553":
+     return await msg.reply_text("**This is not a Forward message or this message was not forward from bot father**")
+  copy = await bot.get_messages(msg.forward_from.id, msg.forward_from_message_id)
   token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', copy.text, re.IGNORECASE)
-  if not token:
-     await msg.reply_text("invalid bot token")
-  print(token)
-  #if token is None:
-  for match in regex.finditer(copy.text):
-     tokens = match 
-  else: 
-     await msg.reply_text(f"error on find bottoken")
-  BOT_TOKEN["test"] = copy.text
+  if token is None:
+     await msg.reply_text("There is no bot token in that message")
+  BOT_TOKEN[m.from_user.id] = copy.text
   await msg.reply_text(f"your token :- \n{token}\nilter :- {tokens}")
   return
-    
