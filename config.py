1caf232e66e430ad4061231900b42cef98b9cef2
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-19 18:43:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) @DarkzzAngel
-
 import os
 import logging
 
 class Config:
     
+    CONFIGS = {}
+    BANNED_USERS = []
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = os.environ.get("DATABASE", "")
     CAPTION = os.environ.get("CAPTION", "")
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "document")
     OWNER_ID = os.environ.get("OWNER_ID", 12345)
     SESSION = os.environ.get("SESSION")
+    
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
