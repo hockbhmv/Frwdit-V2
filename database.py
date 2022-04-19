73beef7e3395301f7967a98482a1a15a9fe21c7f
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-19 15:34:40 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,84 @@
+from os import environ 
+import motor.motor_asyncio
+ 
+DATABASE_URI = environ.get("DATABASE", "")
+
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+        
+
+    def new_user(self, id, name):
+        return dict(
+            id = id,
+            name = name,
+            ban_status=dict(
+                is_banned=False,
+                ban_reason="",
+            ),
+        )
+      
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id':int(id)})
+        return bool(user)
+    
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+    
+    async def remove_ban(self, id):
+        ban_status = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
+    
+    async def ban_user(self, user_id, ban_reason="No Reason"):
+        ban_status = dict(
+            is_banned=True,
+            ban_reason=ban_reason
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
+
+    async def get_ban_status(self, id):
+        default = dict(
+            is_banned=False,
+            ban_reason=''
+        )
+        user = await self.col.find_one({'id':int(id)})
+        if not user:
+            return default
+        return user.get('ban_status', default)
+
+    async def get_all_users(self):
+        return self.col.find({})
+    
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+ 
+    async def get_banned(self):
+        users = self.col.find({'ban_status.is_banned': True})
+        b_users = [user['id'] async for user in users]
+        return 
+
+    async def update_configs(self, id, configs):
+        await self.col.update_one({'id': int(id)}, {'$set': {'configs': configs}})
+         
+    async def get_configs(self, id):
+        default = {
+            'bot_token': None,
+            'db_channels': None
+        }
+        user = await self.col.find_one({'id':int(id)})
+        if user:
+            return user.get('configs', default)
+        return default
+      
+db = Database(DATABASE_URI, "forward bot")
