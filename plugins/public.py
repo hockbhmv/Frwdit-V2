96ed9643c6f272503a41ea48b41884cc6f679cd3
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-24 22:34:12 +0530

    Some changes

--- plugins/public.py
+++ plugins/public.py
@@ -22,7 +22,8 @@ async def run(bot, message):
     if not buttons:
        return await message.reply_text("please set a to channel in /settings before forwarding")
     buttons.append([KeyboardButton("cancel")])
-    _toid = await bot.ask(message.chat.id, Translation.TO_MSG, reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, resize_keyboard=True))
+    bot = await db.get_bot(user_id)
+    _toid = await bot.ask(message.chat.id, Translation.TO_MSG.format(bot['username']), reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, resize_keyboard=True))
     if _toid.text.startswith(('/', 'cancel')):
         await message.reply_text(Translation.CANCEL, reply_markup=ReplyKeyboardRemove())
         return
@@ -65,7 +66,6 @@ async def run(bot, message):
     if skipno.text.startswith('/'):
         await message.reply(Translation.CANCEL)
         return
-    bot = await db.get_bot(user_id)
     forward_id = f"{user_id}-{skipno.message_id}"
     buttons = [[
         InlineKeyboardButton('Yes', callback_data=f"start_public_{forward_id}"),
@@ -73,7 +73,7 @@ async def run(bot, message):
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await message.reply_text(
-        text=Translation.DOUBLE_CHECK.format(f"[{bot['name']}](t.me/{bot['username']})", chat.title, _toid.text, skipno.text, bot['username']),
+        text=Translation.DOUBLE_CHECK.format(bot['name'], bot['username'], chat.title, _toid.text, skipno.text, bot['username']),
         parse_mode="combined",
         disable_web_page_preview=True,
         reply_markup=reply_markup
