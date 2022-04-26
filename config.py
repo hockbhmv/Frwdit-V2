1c67ad17de0af970425698cbd82abf77e518d6ce
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-26 06:38:03 +0530

    Auth Channel

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ class Config:
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = os.environ.get("DATABASE", "")
+    BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
     
 class temp(object): 
     lock = {}
