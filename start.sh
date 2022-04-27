dacc79d2a41ee54e6792d082178f2fb8ad67de99
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-27 15:52:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ then
   echo "Cloning main branch...."
   git clone https://github.com/hockbhmv/Frwdit-V2 /Frwdit-V2 
 else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/hockbhmv/Frwdit-V2 -b $BRANCH /Frwdit-V2 
+  echo "Cloning $REPO_URL using $BRANCH branch...."
+  git clone $REPO_URL -b $BRANCH /$DIR 
 fi
-cd /Frwdit-V2 
+cd /$DIR
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
