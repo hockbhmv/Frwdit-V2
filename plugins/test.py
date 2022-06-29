11cfefd845658d5509177e9d9c4332a395ad2692
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-06-29 22:52:44 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -8,35 +8,36 @@ from config import temp
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, Message 
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait 
+from pyropatch import listen
 from config import Config
 from translation import Translation
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+class CLIENT():
+  def __init__(self, bot_token)
+      bot = Client(":memory:", Config.API_ID, Config.API_HASH, bot_token=bot_token)
+    
 @Client.on_message(filters.private & filters.command('add'))
 async def bot_token(bot, m):
-  msg = await bot.ask(chat_id=m.from_user.id, text="1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me")
+  msg = await bot.ask_message(chat_id=m.from_user.id, text="1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me")
   if not msg.forward_date:
      await msg.reply_text("This is not a forward message")
      return False
   if str(msg.forward_from.id) != "93372553":
      await msg.reply_text("This message was not forward from bot father")
      return False
-  bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
-  if not bot_token and bot_token == []:
+  token = await m.get_bot_token()
+  if not token:
      await msg.reply_text("There is no bot token in that message")
      return False
   try:
-    client = Client(f":memory:", Config.API_ID, Config.API_HASH, bot_token=bot_token[0])
-    await client.start()
-    _bot = await client.get_me()
-  except (AccessTokenExpired, AccessTokenInvalid):
-    await msg.reply_text("The given bot token is invalid")
-    return False
+     _client = await bot.start_clone_bot(CLIENT(token).bot)
   except Exception as e:
     await msg.reply_text(f"Bot Error:- {e}")
     return False
+  _bot = _client.details
   details = {
     'id': _bot.id,
     'name': _bot.first_name,
@@ -44,10 +45,6 @@ async def bot_token(bot, m):
     'username': _bot.username
   }
   await update_configs(m.from_user.id, 'bot', details)
-  try:
-    await client.stop()
-  except:
-    pass
   return True
 
 @Client.on_message(filters.private & filters.command('reset'))
