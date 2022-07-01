18cc953b1707376fc4e4f95c4d00f7ed69a16ba6
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-07-01 17:32:19 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -174,13 +174,13 @@ PROGRESS = """
 ⏳️ETA: {2}
 """
 
-async def edit(msg, title, status, frwd_id, buttons=None):
+async def edit(msg, title, status, frwd_id, button=None):
   # total_files, skip, total, fetched, deleted, filtered, duplicate, start = get(frwd_id, full=True)
  #  current = deleted + total_files + duplicate + filtered + skip                               
   # percentages = "{:.0f}".format(float(current)*100/float(total))
  #  text = TEXT.format(title, fetched, total_files, duplicate, deleted, skip, filtered, percentages)
    text = "hi"
-   if button:
+   if buttons:
         now = time.time()
         diff = now - start
         percentage = current * 100 / total
