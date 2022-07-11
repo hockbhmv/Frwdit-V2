9c76c5e8445e1fd17d194e41dfbe103e9b348151
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-07-11 21:51:58 +0530

    Fix

--- plugins/utils.py
+++ plugins/utils.py
@@ -5,16 +5,18 @@ STATUS = {}
 class STS:
     def __init__(self, id):
         self.id = id
-        self.status = STATUS
-        self.data = self.status[self.id]
-        
+        self.data = STATUS
+    
+    def verify(self):
+        return self.data.get(self.id)
+    
     def store(self, From, to,  skip, limit):
-        self.data = {'FROM': From, 'TO': to, 'total_files': 0, 'skip': skip, 'limit': limit, 'fetched': 0,
+        self.data[self.id] = {'FROM': From, 'TO': to, 'total_files': 0, 'skip': skip, 'limit': limit, 'fetched': 0,
                      'filtered': 0, 'deleted': 0, 'duplicate': 0, 'total': 0, 'current': 0, 'start': time.time()}
         return STS(self.id)
         
     def get(self, value=None, full=False):
-        values = self.status.get(self.id)
+        values = self.data.get(self.id)
         if not full:
            return values.get(value)
         for k, v in values.items():
@@ -23,5 +25,5 @@ class STS:
 
     def add(self, key, value=1, time=False):
         if time:
-          return self.data.update({key: time})
-        self.data.update({key: self.get(key) + value, 'current': self.get('current') + value}) 
+          return self.data[self.id].update({key: time})
+        self.data[self.id].update({key: self.get(key) + value, 'current': self.get('current') + value}) 
